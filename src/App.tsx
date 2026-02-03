6094da914733fbbeb96d2d03be9739e05541a2a9
Alejandro LR <alejandro.lr.developer@hotmail.com>
2026-02-03 16:17:29 -0500

    Add: Shadcn

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,15 @@
 function App() {
   return (
     <div className="min-h-screen bg-[#F6F7F8]">
-      <h1>Hello World!</h1>
+      <header className="sticky top-0 z-10 bg-[#F6F7F8]/80 backdrop-blur-lg border-b border-[#ECECEC]">
+        <div className="max-w-6xl mx-auto px-4 sm:px-6 lg:px-8 py-4">
+          <div className="flex items-center justify-between">
+            <h2 className="text-xl font-bold text-foreground">Monitify</h2>
+
+            <button onClick={() => {}}>Account</button>
+          </div>
+        </div>
+      </header>
     </div>
   )
 }
